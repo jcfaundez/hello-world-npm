4062871cc49b7591618d1b01684b7e3eba118b69
martin <martin@admin.com>
2020-06-21 20:49:49 -0300

    server upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// Include http module.
+var http = require('http');
+
+// Create http server.
+var httpServer = http.createServer(function (req, resp) {
+
+    resp.writeHeader(200);
+
+    resp.end('This is a NodeJs Server ');
+});
+
+// Http server listen on port 8888.
+httpServer.listen(8888);
+
+console.log("Http web server listening on port 8888. Access it with url http://localhost:8888/.");
